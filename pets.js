124b1c8b9ceefd305b9acab8b3b2b8c12264fc6e
Joed-Hirales <94884333+Joed-Hirales@users.noreply.github.com>
2024-04-24 12:52:09 -0700

    first commit

---
+++ pets.js
@@ -0,0 +1,23 @@
+const pets = [
+    {
+        pet_id : 1,
+        name_pet: "Gaia",
+        species_pet: "Felino",
+        breed:"egipcio pelon",
+        birth_date_pet: "20/12/1995",
+        gender_pet: "female",
+        weight_pet: "10kg",
+        owner_id: 1
+      }  ,
+      {
+        pet_id : 2,
+        name_pet: "chichi",
+        species_pet: "Caninos",
+        breed:"Sanbernard",
+        birth_date_pet: "15/10/1998",
+        gender_pet: "Male",
+        weight_pet: "14g",
+        owner_id: 2
+      }
+];
+module.exports = pets;
---
+++ users.js
@@ -0,0 +1,18 @@
+const  users = [
+    {
+        id_user: 1,
+        last_name_user: "Gutierrez",
+        name_user: "Yahir",
+        email_user: "megustanlasquesadillas@getMaxListeners.com",
+        phone_number_user: "6121231231"
+      }  ,
+      {
+        id_user: 2,
+        last_name_user: "Benito",
+        name_user: "lopez",
+        email_user: "megustaelchocomilk@getMaxListeners.com",
+        phone_number_user: "6121231231"
+      }
+];
+
+module.exports = users;
